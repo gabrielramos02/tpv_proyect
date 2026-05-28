ddf1f240b84c2e79c35a03a20757e70535a48999
gabrielramos02 <gabriekrp02ok@outlook.com>
2026-05-28 11:07:27 +0200

    added proyectV2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_proyect"
+    namespace = "com.example.tpv_proyect"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.tpv.proyect"
+        applicationId = "com.tpv.proyectV2"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
